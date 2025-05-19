760d1f2fcdb96fd5ea30c9a87d6748f409e8d9c1
Mike Hovedskov <Deengo113@gmail.com>
2025-05-19 22:11:55 +0200

    click

--- src/components/shared/button/Button.tsx
+++ src/components/shared/button/Button.tsx
@@ -1,9 +1,10 @@
+import type { ButtonProps } from '../../../models/Button'
 import './Button.scss'
 
 
-const Button = ({ icon, text }: { icon?: string, text: string }) => {
+const Button = ({ icon, text, onClick }: ButtonProps) => {
   return (
-    <button className="Button">
+    <button className="Button" onClick={onClick}>
       {icon && (
         <img src={icon} className="Button_Icon" />
       )}
