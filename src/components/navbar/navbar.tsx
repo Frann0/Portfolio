da01f6fa8f29c43645940cead37c766b6d299d32
Mike Hovedskov <Deengo113@gmail.com>
2025-05-19 17:42:28 +0200

    Scroll Progress

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,24 +1,42 @@
+import { useEffect, useState } from 'react';
 import './navbar.scss'
 
 const Navbar = () => {
+  const [scrollProgress, setScrollProgress] = useState(0);
+
+  useEffect(() => {
+    const updateScrollProgress = () => {
+      const scrollTop = window.scrollY;
+      const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+      const progress = (scrollTop / docHeight) * 100;
+      setScrollProgress(progress);
+    };
+
+    window.addEventListener("scroll", updateScrollProgress);
+    return () => window.removeEventListener("scroll", updateScrollProgress);
+  }, []);
+
   return (
-    <nav className="navbar">
-      <div className="navbar_logo">
-        <img src="icons/Planet.svg" className="navbar_logo_icon" alt="Logo" />
-        <p className="navbar_logo_text">Mike Hovedskov</p>
-      </div>
-      <div className="navbar_links">
-        <a href="#about" className="navbar_links_link">
-          About Me
-        </a>
-        <a href="#projects" className="navbar_links_link">
-          Projects
-        </a>
-        <a href="#contact" className="navbar_links_link">
-          Contact
-        </a>
-      </div>
-    </nav>
+    <>
+      <nav className="navbar">
+        <div className="navbar_logo">
+          <img src="icons/Planet.svg" className="navbar_logo_icon" alt="Logo" />
+          <p className="navbar_logo_text">Mike Hovedskov</p>
+        </div>
+        <div className="navbar_links">
+          <a href="#about" className="navbar_links_link">
+            About Me
+          </a>
+          <a href="#projects" className="navbar_links_link">
+            Projects
+          </a>
+          <a href="#contact" className="navbar_links_link">
+            Contact
+          </a>
+        </div>
+      </nav>
+      <div className="Navbar_Progress" style={{ width: `${scrollProgress}%` }}></div>
+    </>
   )
 }
 
